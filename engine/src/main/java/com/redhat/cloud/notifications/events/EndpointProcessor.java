547b11f830035f473722a42a4774dbc4e1de7dc4
Gwenneg Lepage <glepage@redhat.com>
2022-12-12 23:57:47 +0100

    RHCLOUD-22545

--- engine/src/main/java/com/redhat/cloud/notifications/events/EndpointProcessor.java
+++ engine/src/main/java/com/redhat/cloud/notifications/events/EndpointProcessor.java
@@ -5,9 +5,9 @@ import com.redhat.cloud.notifications.db.repositories.EndpointRepository;
 import com.redhat.cloud.notifications.models.Endpoint;
 import com.redhat.cloud.notifications.models.EndpointType;
 import com.redhat.cloud.notifications.models.Event;
-import com.redhat.cloud.notifications.processors.EndpointTypeProcessor;
 import com.redhat.cloud.notifications.processors.camel.CamelTypeProcessor;
 import com.redhat.cloud.notifications.processors.email.EmailSubscriptionTypeProcessor;
+import com.redhat.cloud.notifications.processors.rhose.RhoseTypeProcessor;
 import com.redhat.cloud.notifications.processors.webhooks.WebhookTypeProcessor;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
@@ -30,13 +30,16 @@ public class EndpointProcessor {
     EndpointRepository endpointRepository;
 
     @Inject
-    WebhookTypeProcessor webhooks;
+    WebhookTypeProcessor webhookProcessor;
 
     @Inject
-    CamelTypeProcessor camel;
+    CamelTypeProcessor camelProcessor;
 
     @Inject
-    EmailSubscriptionTypeProcessor emails;
+    RhoseTypeProcessor rhoseProcessor;
+
+    @Inject
+    EmailSubscriptionTypeProcessor emailProcessor;
 
     @Inject
     MeterRegistry registry;
@@ -52,35 +55,53 @@ public class EndpointProcessor {
 
     public void process(Event event) {
         processedItems.increment();
+
         List<Endpoint> endpoints = endpointRepository.getTargetEndpoints(event.getOrgId(), event.getEventType());
+        endpointTargeted.increment(endpoints.size());
 
         // Target endpoints are grouped by endpoint type.
-        endpointTargeted.increment(endpoints.size());
         Map<EndpointType, List<Endpoint>> endpointsByType = endpoints.stream().collect(Collectors.groupingBy(Endpoint::getType));
 
         DelayedThrower.throwEventually(DELAYED_EXCEPTION_MSG, accumulator -> {
             for (Map.Entry<EndpointType, List<Endpoint>> entry : endpointsByType.entrySet()) {
                 try {
-                    // For each endpoint type, the list of target endpoints is sent alongside with the event to the relevant processor.
-                    EndpointTypeProcessor processor = endpointTypeToProcessor(entry.getKey());
-                    processor.process(event, entry.getValue());
+                    // TODO Create a new endpoint type for RHOSE.
+                    if (EndpointType.CAMEL.equals(entry.getKey())) {
+                        Map<String, List<Endpoint>> b = entry.getValue().stream().collect(Collectors.groupingBy(Endpoint::getSubType));
+                        for (Map.Entry<String, List<Endpoint>> a : b.entrySet()) {
+                            try {
+                                switch (a.getKey()) {
+                                    case "slack":
+                                        rhoseProcessor.process(event, entry.getValue());
+                                        break;
+                                    default:
+                                        camelProcessor.process(event, entry.getValue());
+                                        break;
+                                }
+                            } catch (Exception e) {
+                                accumulator.add(e);
+                            }
+                        }
+                    } else {
+                        // For each endpoint type, the list of target endpoints is sent alongside with the event to the relevant processor.
+                        switch (entry.getKey()) {
+                            case CAMEL:
+                                camelProcessor.process(event, entry.getValue());
+                                break;
+                            case EMAIL_SUBSCRIPTION:
+                                emailProcessor.process(event, entry.getValue());
+                                break;
+                            case WEBHOOK:
+                                webhookProcessor.process(event, entry.getValue());
+                                break;
+                            default:
+                                throw new IllegalArgumentException("Unexpected endpoint type: " + entry.getKey());
+                        }
+                    }
                 } catch (Exception e) {
                     accumulator.add(e);
                 }
             }
         });
-    }
-
-    private EndpointTypeProcessor endpointTypeToProcessor(EndpointType endpointType) {
-        switch (endpointType) {
-            case CAMEL:
-                return camel;
-            case WEBHOOK:
-                return webhooks;
-            case EMAIL_SUBSCRIPTION:
-                return emails;
-            default:
-                throw new IllegalArgumentException("Unexpected endpoint type: " + endpointType);
-        }
     }
 }
